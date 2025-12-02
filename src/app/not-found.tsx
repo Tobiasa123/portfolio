c38b103f3015571b6dd347108dc73104a647fd6c
Tobias Andersson <tobiaserikandersson@gmail.com>
2025-12-02 18:51:54 +0200

    simple not-found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+// app/not-found.tsx
+export default function NotFound() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p className="mt-4">Oops! Page not found...</p>
+    </div>
+  );
+}
