683f9601a163b5fb3278007c0228fa4c46d4aee5
Tobias Andersson <tobiaserikandersson@gmail.com>
2025-12-15 12:25:39 +0200

    translated authform

--- src/app/(public)/[locale]/login/page.tsx
+++ src/app/(public)/[locale]/login/page.tsx
@@ -1,19 +1,8 @@
-//src/app/(public)/[locale]/login/page.tsx
+// src/app/(public)/[locale]/login/page.tsx
 "use client";
 import { AuthForm } from "@/components/AuthForm";
 
 export default function LoginPage() {
-  return (
-    <AuthForm
-      title="Log in"
-      submitLabel="Log In"
-      loadingLabel="Logging in..."
-      endpoint="/api/auth/login"
-      redirectTo="/dashboard"
-      bottomLink={{
-        label: "Not registered? Sign up here.",
-        href: "/register",
-      }}
-    />
-  );
+  return <AuthForm type="login" redirectTo="/dashboard" />;
 }
+
--- src/app/(public)/[locale]/register/page.tsx
+++ src/app/(public)/[locale]/register/page.tsx
@@ -1,21 +1,8 @@
 
-//src/app/(public)/[locale]/register/page.tsx
-
+// src/app/(public)/[locale]/register/page.tsx
 "use client";
 import { AuthForm } from "@/components/AuthForm";
 
 export default function RegisterPage() {
-  return (
-    <AuthForm
-      title="Sign Up"
-      submitLabel="Sign Up"
-      loadingLabel="Registering..."
-      endpoint="/api/auth/register"
-      redirectTo="/login"
-      bottomLink={{
-        label: "Already registered? Log in here.",
-        href: "/login",
-      }}
-    />
-  );
+  return <AuthForm type="register" redirectTo="/login" />;
 }
